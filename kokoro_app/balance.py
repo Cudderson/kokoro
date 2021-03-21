51bc4a94d2e15ad9c5fbc10117d4e0bdcff058a3
cudderson <codered1227@gmail.com>
2021-03-20 22:21:46 -0500

    balance feature implemented successfully (rough)

--- kokoro_app/balance.py
+++ kokoro_app/balance.py
@@ -1,17 +1,14 @@
-# File for 'balance' feature helper functions/calculations
+# File for 'balance' feature helper functions/queries
 
 from kokoro_app.models import Activity
 import datetime
 
+# Get all activities submitted today for a logged-in user
+
 
 def daily_mind(request):
     """Returns all mind-related activites submitted today for user"""
-    # Get data only from 12:00AM (today)
 
-    # this works, let's use this for now. The only problem is warning that the object is naive, not aware
-    # a = Activity.objects.filter(date_added__gte=datetime.datetime.today())
-
-    # return activities related to the mind that were submitted today
     daily_mind_activities = Activity.objects.filter(
         owner__username=request.user,
         activity__iexact='mind',
@@ -46,7 +43,20 @@ def daily_soul(request):
 
 
 def balance(request):
-    """Helper function for balance feature on home page"""
-    ...
+    """
+    Returns a boolean indicating if user has at least 1 activity for mind, body and soul
+    """
 
+    # Determine if user has submitted at least 1 activity for mind, body and soul
+    mind_fulfilled = True if len(daily_mind(request)) > 0 else False
+    body_fulfilled = True if len(daily_body(request)) > 0 else False
+    soul_fulfilled = True if len(daily_soul(request)) > 0 else False
 
+    # If all true, user has found balance
+    if mind_fulfilled and body_fulfilled and soul_fulfilled:
+        found_balance = True
+    else:
+        found_balance = False
+
+    # Convert bool to string for template evaluation (Jinja2)
+    return str(found_balance)
--- kokoro_app/views.py
+++ kokoro_app/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Activity
 from .forms import ActivityForm
-
 from . import balance
 
 
@@ -29,9 +28,13 @@ def home(request):
 
     activities = Activity.objects.filter(owner=request.user).order_by('date_added')
 
+    # Returns all activities submitted today for user
     daily_mind = balance.daily_mind(request)
     daily_body = balance.daily_body(request)
     daily_soul = balance.daily_soul(request)
+
+    # Returns boolean indicating if user has submitted at least 1 mind, body, and soul activity today
+    found_balance = balance.balance(request)
 
     context = {
         'form': form,
@@ -39,6 +42,7 @@ def home(request):
         'daily_mind': daily_mind,
         'daily_body': daily_body,
         'daily_soul': daily_soul,
+        'balance_bool': found_balance,
     }
 
     return render(request, 'kokoro_app/home.html', context)
