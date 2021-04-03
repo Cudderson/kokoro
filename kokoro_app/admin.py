744695d023e4f3e0dd7c091a4dc3e81c0e877f95
cudderson <codered1227@gmail.com>
2021-04-02 22:17:18 -0500

    register PerfectBalance model with admin-panel

--- kokoro_app/admin.py
+++ kokoro_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Activity
+from .models import Activity, PerfectBalance
 
 admin.site.register(Activity)
+admin.site.register(PerfectBalance)
