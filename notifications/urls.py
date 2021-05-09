9cae4f1f0e58a7c8a3619801fd98749530ca5b9f
cudderson <codered1227@gmail.com>
2021-05-09 11:53:48 -0500

    begin logic for handling forms involving Notification objects

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,1 +1,14 @@
-# might not use
+""" Defines URL patterns for notifications app """
+
+from django.urls import path
+
+from . import views
+
+app_name = 'notifications'
+
+urlpatterns = [
+
+    # Called when a Notification object needs to be altered
+    path('notification_form_handler/', views.notification_form_handler, name='notification_form_handler'),
+
+]
--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def notification_form_handler(request):
+    """
+    Helper function for handling forms involving Notification model
+    :param request: http data
+    :return:
+    """
+
+    if request.method == 'POST':
+        # do stuff
+        ...
+
+    return redirect('/profile')
