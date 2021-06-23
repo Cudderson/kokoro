583e798eada02d28b739ef16bdb0dbc86bbaba48
cudderson <codered1227@gmail.com>
2021-06-23 12:24:38 -0500

    try explicitly setting EMAIL_PORT to avoid possible int>>str conversion on heroku

--- kokoro/settings.py
+++ kokoro/settings.py
@@ -194,9 +194,11 @@ LOGIN_URL = 'users:login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('KOKORO_EMAIL_HOST')
 EMAIL_USE_TLS = True
-EMAIL_PORT = os.environ.get('KOKORO_EMAIL_PORT')
+# EMAIL_PORT = os.environ.get('KOKORO_EMAIL_PORT')
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('KOKORO_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('KOKORO_EMAIL_HOST_PASSWORD')
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 EMAIL_VARS = [
     EMAIL_HOST,
